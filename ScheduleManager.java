a68f1a474fb71174c998a7b01ee98ce619267216
edita-khasapetyan <edita_khasapetyan@edu.aua.am>
2025-05-05 14:06:39 +0400

    Update ScheduleManager.java

--- ScheduleManager.java
+++ ScheduleManager.java
@@ -1,1 +1,41 @@
-﻿
+﻿import java.util.ArrayList;
+public class ScheduleManager {
+  private ArrayList<Schedule> schList;
+
+    public ScheduleManager() {
+        schList = new ArrayList<>();
+    }
+  public void addSchedule(Schedule s) {
+        schList.add(s);
+    }
+   public void removeSchedule(String name) {
+        for (int i = 0; i < schList.size(); i++) {
+            if (schList.get(i).getName().equalsIgnoreCase(name)) {
+                schList.remove(i); 
+              System.out.println("You removed the event:" + name);
+            }
+          else {
+            System.out.println("There is no such event with the name:" + name);
+          }
+        }
+   }
+  public Schedule getSchedule(String name) {
+    for (Schedule s : schList) {
+        if (s.getName().equalsIgnoreCase(name)) {
+            return s;
+        }
+    }
+    return null;
+}
+   public void listSchedules() {
+        if (schedules.isEmpty()) {
+            System.out.println("No schedules available.");
+        } else {
+            System.out.println("Schedules:");
+            for (Schedule s : schedules) {
+                System.out.println("- " + s.getName());
+            }
+        }
+    }
+}
+
